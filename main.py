24bc5ebc55ce06d947038d361adf773dbfdeb5a8
metaabhay <meta29abhay@gmail.com>
2026-04-15 11:17:49 +0530

    Refactor main.py to improve code structure

--- main.py
+++ main.py
@@ -136,9 +136,8 @@ with tab1:
             if df[col].dtype == "object":
                 avg = df.groupby(col)[target].mean()
             else:
-                bins = pd.cut(df[col], bins=5, include_lowest=True)
-                avg = df.groupby(bins)[target].mean()
-                avg.index = [f"{max(0, round(i.left,1))} to {round(i.right,1)}" for i in avg.index]
+                bins = pd.cut(df[col], bins=[0,2,4,6,8,10])
+                avg.index = ["0-2", "2-4", "4-6", "6-8", "8-10"]
 
             sns.barplot(x=avg.index, y=avg.values, ax=ax)
             ax.set_xlabel(col)
